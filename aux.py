f45982c0e0bd10698ca0d3fcdcd83bb26dcce5d6
Anton Buyskikh <anton.buyskikh@gmail.com>
2019-06-25 00:37:32 +0100

    Outline part1

--- aux.py
+++ aux.py
@@ -8,4 +8,28 @@ import sqlite3
 
 # %% functions
 
+def create_table():
+  """ Create a table.
+  """
+  pass
 
+
+
+def insert_into_table():
+  """ Insert data into a table.
+  """
+  pass
+
+
+
+def select_from_table():
+  """ Seletc data from a table.
+  """
+  pass
+
+
+
+def add_column():
+  """ Add column in a table.
+  """
+  pass
--- part1.py
+++ part1.py
@@ -10,5 +10,30 @@
 import sqlite3
 import aux
 
-# %% main part
+# %% create a database with a table
 
+db_filename = 'geometric_shapes.db'
+
+aux.create_table()
+
+# ACTION: show that the table is created in the termial
+
+# %% add data in the table
+
+data = dict(type='rectangle', height=1.0, width=1.0, colour='red')
+
+aux.insert_into_table()
+
+# ACTION: show that the data is in the table
+
+# %% add more data
+
+aux.insert_into_table()
+aux.insert_into_table()
+
+for height in [5.4, 3.7, 2.8]:
+  for width in [0.5, 1.4, 3.6]:
+    for colour in ['blue', 'black', 'grenn']:
+      aux.insert_into_table()
+      
+# ACTION: show that the data is in the table
