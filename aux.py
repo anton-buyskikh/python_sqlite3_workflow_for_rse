5a6ea970564f660d1823c6cdf01f5064c4a9634b
Anton Buyskikh <anton.buyskikh@gmail.com>
2019-06-25 11:21:27 +0100

    Add select_from_table_where

--- aux.py
+++ aux.py
@@ -25,9 +25,10 @@ def create_table_geometric_shapes(db_filename):
   cur = con.cursor()
   cur.execute(""" create table if not exists geometric_shapes(
                   shape text not null,
+                  colour text not null,
                   height real,
                   width real,
-                  colour text not null,
+                  area real,
                   datetime text not null); """)
   con.commit()
   con.close()
@@ -60,10 +61,58 @@ def insert_into_geometric_shapes(db_filename, data):
 
 
 
-def select_from_table():
-  """ Seletc data from a table.
+def select_from_table_where(db_filename,
+                            conditions,
+                            column_list=['*']):
+  """ Return data from a table with conditions
+  
+  Inputs
+  ------
+  db_filename : str
+    database filename
+  conditions : dict
+    dictionary with arguments
+  column_list : list of str
+    columns, by default return all columns
+    
+  Outputs
+  -------
+  output : list
+    raws with data 
   """
-  pass
+  condition_pairs = build_par_pairs(conditions)
+  column_names = ', '.join(column_list)
+  
+  con = sqlite3.connect(db_filename)
+  cur = con.cursor()
+  output = cur.execute('select ' + column_names +
+                        ' from geometric_shapes' +
+                        ' where ' + ' and '.join(condition_pairs) + ';').fetchall()
+  con.close()
+  return output
+
+
+
+def build_par_pairs(pars):
+  """ Return the list of parameter pairs in a form of strings
+  
+  Inputs
+  ------
+  pars : dict
+    dictionary with arguments
+  
+  Outputs
+  -------
+  par_pairs : list of str
+    parameter pairs in strings
+  """
+  par_pairs = []
+  for key in pars.keys():
+    if type(pars[key]) == str:
+      par_pairs.append(key + '="' + pars[key] + '"')
+    else:
+      par_pairs.append(key + '=' + str(pars[key]))
+  return par_pairs
 
 
 
