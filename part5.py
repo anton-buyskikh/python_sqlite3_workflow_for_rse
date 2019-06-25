ab0689dbb9f29379aa9b9561f6f0d7857d2dff9e
Anton Buyskikh <anton.buyskikh@gmail.com>
2019-06-25 16:03:42 +0100

    Edit part5

--- part5.py
+++ part5.py
@@ -1,1 +1,5 @@
-about numpy.array connector
+#
+# Part V: extra (UNFINISHED)
+# 1. save any object in the table by creating a `connector`, e.g. numpy.array
+# 2. for data version control save git_hash in the table
+#
