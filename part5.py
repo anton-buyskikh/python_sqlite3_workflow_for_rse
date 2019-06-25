4ee0376c29165a9f8385950040ee82456b4f5389
Anton Buyskikh <anton.buyskikh@gmail.com>
2019-06-25 17:58:01 +0100

    Add slides and update part5

--- part5.py
+++ part5.py
@@ -2,4 +2,7 @@
 # Part V: extra (UNFINISHED)
 # 1. save any object in the table by creating a `connector`, e.g. numpy.array
 # 2. for data version control save git_hash in the table
+# 3. the database can be updated by one process at a time, hence while running
+#    simulations on a cluster with multiple simultaneous jobs the waiting time
+#    of the connection has to be increased to several minutes.
 #
